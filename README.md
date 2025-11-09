617ebb67f932a2ea8afb7b1d3220b2e95720e06d
hp0912 <809211365@qq.com>
2025-11-09 12:05:13 +0800

    feat: 环境变量

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wechat-robot-mcp-server
+
+### 启动服务
+
+```
+go run .
+```
+
+### 打包编译
+
+```
+go build .
+```
--- config.go
+++ config.go
@@ -28,14 +28,15 @@ func loadEnvConfig() {
 		}
 	}
 
-	MCPServerPort, err := strconv.Atoi(os.Getenv("MCP_SERVER_PORT"))
+	port, err := strconv.Atoi(os.Getenv("MCP_SERVER_PORT"))
 	if err != nil {
-		log.Fatal("加载本地环境变量失败，请检查是否存在 .env 文件")
+		log.Fatal("环境变量 [MCP_SERVER_PORT] 未配置")
 	}
-	if MCPServerPort == 0 {
-		MCPServerPort = 9000
+	if port == 0 {
+		port = 9000
 	}
-	if MCPServerPort < 1 || MCPServerPort > 65535 {
+	if port < 1 || port > 65535 {
 		log.Fatal("MCPServerPort 必须在 1 到 65535 之间")
 	}
+	MCPServerPort = port
 }
